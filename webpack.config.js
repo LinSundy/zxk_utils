e9d169fdab8414cad5f7533653d7f76eeae7d7e2
车林 <chelin@bcbook.cn>
2021-01-19 08:02:36 +0800

    试验使用webpack打包ts， 并且将ts打包为cjs的格式，可以在node的环境中运行

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require('path')
 
 module.exports = {
-    mode: "development",
-    entry: './src/a.js',
+    mode: "production",
+    entry: './src/index.ts',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    }
+        filename: '[name].js'
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
 }
